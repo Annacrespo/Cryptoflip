83524b172b9609b515e938e3b1e7c35d851ace7f
JonH <jheal006@gmail.com>
2017-10-02 12:20:02 -0700

    set up connection.js

---
+++ server.js
@@ -0,0 +1,39 @@
+// *********************************************************************************
+// Server.js - This file is the initial starting point for the Node/Express server.
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+// Static directory to be served
+app.use(express.static("app/public"));
+
+// Routes
+// =============================================================
+require("./app/routes/api-routes.js")(app);
+
+// Here we introduce HTML routing to serve different HTML files
+require("./app/routes/html-routes.js")(app);
+
+
+// Syncing our sequelize models and then starting our Express app
+// =============================================================
+// db.sequelize.sync().then(function() {
+    app.listen(PORT, function() {
+      console.log("App listening on PORT " + PORT);
+    });
+//   });
+  
