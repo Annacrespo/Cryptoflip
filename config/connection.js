c3c81f7d98bae868f2552f306a79a4a7597db6f6
jheal006 <jheal006@gmail.com>
2017-10-03 16:05:32 -0700

    Update connection.js

--- config/connection.js
+++ config/connection.js
@@ -6,7 +6,7 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
-var sequelize = new Sequelize("cryptoflip_db", "root", "hkg36c", {
+var sequelize = new Sequelize("cryptoflip_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
   pool: {
