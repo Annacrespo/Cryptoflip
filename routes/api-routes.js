50655cd0ceb6ed662288f0db68eaae485c8c8a5c
fneim <neimfargin@gmx.com.tr>
2017-10-03 10:09:41 -0700

    fixed footer again

---
+++ routes/api-routes.js
@@ -0,0 +1,17 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+var app = express();
+var port = 8080;
+
+app.use(bodyParser.json());
+app.use(express.static(__dirname + 'public'));
+
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, "../views/view.html"));
+})
+
+app.listen(port, function() {
+  console.log("Listening on port " + port);
+})
