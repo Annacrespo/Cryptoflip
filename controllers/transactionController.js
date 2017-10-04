0c842a6289deddb4c0549357c6d67d6cd1b2182f
jheal006 <jheal006@gmail.com>
2017-10-03 17:15:07 -0700

    add transaction controller file

---
+++ controllers/transactionController.js
@@ -0,0 +1,52 @@
+// =============================================================
+// Dependencies
+// =============================================================
+
+var Transaction = require("../models/transactions.js");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+  // Get all transactions
+  app.get("/api/transaction", function(req, res) {
+    // Finding all transaction, and then returning them to the user as JSON.
+    // Sequelize queries are aynchronous, which helps with percieved speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    Transaction.findAll({}).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+  });
+
+  app.post("/api/transaction", function(req, res) {
+    // Finding all transaction, and then returning them to the user as JSON.
+    // Sequelize queries are aynchronous, which helps with percieved speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+    console.log(req.body);
+    Transaction.create(req.body).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+  });
+
+  app.delete("/api/transaction", function(req, res) {
+    // Finding all transaction, and then returning them to the user as JSON.
+    // Sequelize queries are aynchronous, which helps with percieved speed.
+    // If we want something to be guaranteed to happen after the query, we'll use
+    // the .then function
+
+    Transaction.destroy({
+      where: {
+        currency: req.body.currency
+      }
+    }).then(function(results) {
+      // results are available to us inside the .then
+      res.json(results);
+    });
+  });
+
+
+
+};
--- db/schema.sql
+++ db/schema.sql
@@ -4,16 +4,16 @@ DROP DATABASE IF EXISTS cryptoflip_db;
 CREATE DATABASE cryptoflip_db;
 USE cryptoflip_db;
 
-CREATE TABLE transactions
-(
-	id int NOT NULL AUTO_INCREMENT,
-	currency VARCHAR(20),
-  coinsowned INTEGER,
-	buyingpower INTEGER,
-	amtpurchased INTEGER,
-	avgcost INTEGER,
-	equityvalue INTEGER,
-	PRIMARY KEY (id)
-);
- KEY (id)
-);
+-- CREATE TABLE transactions
+-- (
+-- 	id int NOT NULL AUTO_INCREMENT,
+-- 	currency VARCHAR(20),
+--   coinsowned INTEGER,
+-- 	buyingpower INTEGER,
+-- 	amtpurchased INTEGER,
+-- 	avgcost INTEGER,
+-- 	equityvalue INTEGER,
+-- 	PRIMARY KEY (id)
+-- );
+--  KEY (id)
+-- );
