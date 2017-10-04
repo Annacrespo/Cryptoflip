93638f3ec3ac016c263217e6c07957c6ae961b5e
JonH <jheal006@gmail.com>
2017-10-04 11:35:09 -0700

    REACTOR or the transaction routes and using config.json and index.js from the sequelize-cli

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -32,33 +32,4 @@ module.exports = function(app) {
       res.json(results);
     });
   });
-
-<<<<<<< HEAD
-app.get("/view-history", function(req, res) {
-  res.sendFile(path.join(__dirname, "../views/history.html"));
-})
-
-app.listen(port, function() {
-  console.log("Listening on port " + port);
-})
-=======
-  app.delete("/api/transaction", function(req, res) {
-    // Finding all transaction, and then returning them to the user as JSON.
-    // Sequelize queries are aynchronous, which helps with percieved speed.
-    // If we want something to be guaranteed to happen after the query, we'll use
-    // the .then function
-
-    Transaction.destroy({
-      where: {
-        currency: req.body.currency
-      }
-    }).then(function(results) {
-      // results are available to us inside the .then
-      res.json(results);
-    });
-  });
-
-
-
 };
->>>>>>> e31a05e674197d6ec089cfbddae400a30d5ce610
