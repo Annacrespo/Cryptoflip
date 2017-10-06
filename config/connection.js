87d3c4f44641c4095c9c78d3f24bd3e2e6b1063d
annacrespo12 <annacrespo12@gmail.com>
2017-10-05 21:55:59 -0700

    moment http console error update

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,10 @@
-// const mysql = require("mysql");
-// const connection;
-
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
     connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
-        password: '',
+        password: 'shperuby098',
         database: 'cryptoflip_db'
 });
 };
--- public/assets/js/chart.js
+++ public/assets/js/chart.js
@@ -1,108 +1,99 @@
 $(document).ready(function(){
-
-  var buyingPower = parseInt($("#balance").text());
-  var coinsOwned = parseInt($("#quantity-owned").text());
-  var currentPrice = parseInt($("#current-price").text());
-
-  $("#buy").click(function(event) {
-    event.preventDefault();
-
-    var quantity = $("#quantity-buy").val();
-    var totalCost = quantity * currentPrice;
-    var newBalance = buyingPower - totalCost;
-    console.log(totalCost);
-
-    var newTransaction = {
-      quantityInput: quantity,
-      buyingPower: newBalance,
-      totalCost: totalCost,
-      coinsOwned: coinsOwned,
-      currentPrice: currentPrice
-    };
-
-      console.log(newTransaction);
-
-      $.post("/api/transaction", newTransaction)
+  
+  
+  
+    $("#buy").click(function(event) {
+      event.preventDefault();
+      var buyingPower = parseInt($("#balance").text());
+      var coinsOwned = parseInt($("#quantity-owned").text());
+      var currentPrice = parseInt($("#current-price").text());
+  
+      var quantity = parseInt($("#quantity-owned").text()) + parseInt($("#quantity-buy").val())
+      var newCoins = coinsOwned + quantity;
+      var totalCost = quantity * currentPrice;
+      var newBalance = buyingPower - totalCost;
+      console.log(totalCost);
+  
+      var newTransaction = {
+        quantityInput: quantity,
+        buyingPower: newBalance,
+        totalCost: totalCost,
+        coinsOwned: newCoins,
+        currentPrice: currentPrice
+      };
+  
+        console.log(newTransaction);
+  
+        $.post("/api/transaction", newTransaction)
+        .done(function(data) {
+          console.log(data);
+        });
+  
+        $("#quantity-buy").val("");
+    });
+  
+    $("#sell").click(function(event) {
+      var quantityToSell = parseInt($("#quantity-sell").val());
+      var totalSales = quantityToSell * currentPrice;
+      var newBalance = buyingPower + totalSales;
+  
+      var newSale = {
+        quantityInput: quantityToSell,
+        buyingPower: newBalance,
+        totalCost: totalSales,
+        coinsOwned: coinsOwned,
+        currentPrice: currentPrice
+      }
+  
+      $.post("/api/transaction", newSale)
       .done(function(data) {
         console.log(data);
       });
+  
+      $("#quantity-sell").val("");
+  
+    })
+  
+  
+    coinArray = ["BTC", "NEO", "ETH", "ZEC", "LTC"];
+  
 
-      $("#quantity-buy").val("");
-  });
-
-  $("#sell").click(function(event) {
-    var quantityToSell = parseInt($("#quantity-sell").val());
-    var totalSales = quantityToSell * currentPrice;
-    var newBalance = buyingPower + totalSales;
-
-    var newSale = {
-      quantityInput: quantityToSell,
-      buyingPower: newBalance,
-      totalCost: totalSales,
-      coinsOwned: coinsOwned,
-      currentPrice: currentPrice
-    }
-
-    $.post("/api/transaction", newSale)
-    .done(function(data) {
-      console.log(data);
-    });
-
-    $("#quantity-sell").val("");
-
-  })
-
-
-  coinArray = ["BTC", "NEO", "ETH", "ZEC", "LTC"];
-
-  // <<<<<<< HEAD
-  var queryURL = "https://min-api.cryptocompare.com/data/histoday?fsym="+ coinArray[0] + "&tsym=USD&limit=60&aggregate=3&e=CCCAGG";
-
-  //LEAVE STRING ARRAY SET VALUE OF VARIABLE WITHIN QUERY JQUERY SELECT .ONCLICK FUNCTION WILL BE QUERY = ARRAY
-  // =======
-  // var queryURL = "https://min-api.cryptocompare.com/data/histominute?fsym="+coinArray[0]+"&tsym=USD&limit=60&aggregate=3&e=CCCAGG";
-  // <<<<<<< HEAD
-
-  // =======
-
-  // >>>>>>> 3fbd5a10066ba787bad529b74178f104e588b00d
-  //LEAVE STRING ARRAY SET VALUE OF VARIABLE WITHIN QUERY JQUERY SELECT .ONCLICK FUNCTION WILL BE QUERY = ARRAY
-  // >>>>>>> b23c1f6296c70a8f10bfecb95c756b2544a1e0a1
-
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    console.log(response);
-    var dataPoints = [];
-    var labels = [];
-    var data = response.Data;
-
-    for (var i = 0; i < data.length; i++) {
-      var x = moment.unix(data[i].time).format("MM/DD/YY");
-      var y = data[i].close;
-      labels.push(x);
-      dataPoints.push(y);
-    }
-
-  new Chart(document.getElementById("line-chart"), {
-    type: 'line',
-      data: {
-        labels: labels,
-        datasets: [{
-          data: dataPoints,
-          label: "BITCOIN",
-          borderColor: "#3e95cd",
-          fill: false
-        }
-      ]
-    },
-    options: {
-      title: {
-        display: true,
-        text: 'Bitcoin Market'
+    var queryURL = "https://min-api.cryptocompare.com/data/histoday?fsym="+ coinArray[0] + "&tsym=USD&limit=60&aggregate=3&e=CCCAGG";
+  
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).done(function(response) {
+      console.log(response);
+      var dataPoints = [];
+      var labels = [];
+      var data = response.Data;
+  
+      for (var i = 0; i < data.length; i++) {
+        var x = moment.unix(data[i].time).format("MM/DD/YY");
+        var y = data[i].close;
+        labels.push(x);
+        dataPoints.push(y);
       }
-    }
+  
+    new Chart(document.getElementById("line-chart"), {
+      type: 'line',
+        data: {
+          labels: labels,
+          datasets: [{
+            data: dataPoints,
+            label: "BITCOIN",
+            borderColor: "#3e95cd",
+            fill: false
+          }
+        ]
+      },
+      options: {
+        title: {
+          display: true,
+          text: 'Bitcoin Market'
+        }
+      }
+    });
+    })
   });
-  })
-});
